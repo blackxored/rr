23014f0fb32b35643a2e37563cc1ecb28c1dfded
Elliot Winkler <elliot.winkler@gmail.com>
2013-07-14 03:04:50 -0600

    WIP: Convert test_unit_200_rails_3

--- spec/suites/rspec_2_rails_3/integration/test_unit_200_rails_3_spec.rb
+++ spec/suites/rspec_2_rails_3/integration/test_unit_200_rails_3_spec.rb
@@ -1,66 +1,145 @@
 require File.expand_path('../../spec_helper', __FILE__)
-require File.expand_path('../../../common/adapter_integration_tests', __FILE__)
 require File.expand_path('../../../common/rails_integration_tests', __FILE__)
+require File.expand_path('../../../common/rails_test_unit_project', __FILE__)
+require File.expand_path('../../../common/cucumber_project', __FILE__)
 
 describe 'Integration with Test::Unit 2.0.0 and Rails 3' do
-  def adapter_name
-    'test_unit_2_rails_3'
-  end
-
-  def test_framework_path
-    'test/unit'
-  end
-
-  def before_require_test_framework
-    <<-EOT
-      require 'rails'
-      require 'active_support'
-    EOT
-  end
-
-  def error_test
-    with_bootstrap <<-EOT
-      class FooTest < ActiveSupport::TestCase
-        def test_foo
-          object = Object.new
-          mock(object).foo
-        end
-      end
-    EOT
-  end
-
-  def include_adapter_test
-    with_bootstrap <<-EOT
-      class ActiveSupport::TestCase
-        include RR::Adapters::TestUnit
-      end
-
-      class FooTest < ActiveSupport::TestCase
-        def test_foo
-          object = Object.new
-          mock(object).foo
-          object.foo
-        end
-      end
-    EOT
-  end
-
-  def include_adapter_where_rr_included_before_test_framework_test
-    with_bootstrap <<-EOT, :include_rr_before => true
-      class ActiveSupport::TestCase
-        include RR::Adapters::TestUnit
-      end
-
-      class FooTest < ActiveSupport::TestCase
-        def test_foo
-          object = Object.new
-          mock(object).foo
-          object.foo
-        end
-      end
-    EOT
-  end
-
-  include AdapterIntegrationTests
   include RailsIntegrationTests
+
+  def configure_project_generator(project_generator)
+    super
+    project_generator.mixin RailsTestUnitProject
+    project_generator.configure do |project|
+      project.rails_version = 3
+    end
+  end
+
+  def self.including_the_adapter_manually_works
+    specify "including the adapter manually works" do
+      project = generate_project
+      project.add_test_file do |file|
+        file.add_to_body <<-EOT
+          class ActiveSupport::TestCase
+            include RR::Adapters::TestUnit
+          end
+        EOT
+        file.add_working_test_case_with_adapter_tests do |test_case|
+          test_case.add_to_body <<-EOT
+            def test_the_correct_adapters_are_loaded
+              assert_adapters_loaded #{matching_adapters.inspect}
+            end
+          EOT
+        end
+      end
+      result = project.run_tests
+      result.should be_success
+      result.should have_no_errors_or_failures
+    end
+  end
+
+  def self.rr_hooks_into_the_test_framework_automatically
+    specify "RR hooks into the test framework automatically" do
+      project = generate_project
+      project.add_test_file do |file|
+        file.add_working_test_case
+      end
+      result = project.run_tests
+      result.should be_success
+      result.should have_no_errors_or_failures
+    end
+  end
+
+  def self.using_rr_with_cucumber_works
+    specify "using RR with Cucumber works" do
+      project_generator = build_project_generator do |project_generator|
+        project_generator.mixin CucumberProject
+      end
+      project = project_generator.call
+      result = project.run_command_within("bundle exec cucumber")
+      result.should be_success
+    end
+  end
+
+  context 'when Bundler is autorequiring RR' do
+    def configure_project_generator(project_generator)
+      super
+      project_generator.configure do |project|
+        project.autorequire_gems = true
+      end
+    end
+
+    def matching_adapters
+      [:TestUnit200]
+    end
+
+    including_the_adapter_manually_works
+    using_rr_with_cucumber_works
+  end
+
+  context 'when RR is being required manually' do
+    def configure_project_generator(project_generator)
+      super
+      project_generator.configure do |project|
+        project.autorequire_gems = false
+      end
+    end
+
+    def matching_adapters
+      [:TestUnit200, :TestUnit200ActiveSupport, :MiniTest4,
+        :MiniTest4ActiveSupport]
+    end
+
+    rr_hooks_into_the_test_framework_automatically
+    including_the_adapter_manually_works
+    using_rr_with_cucumber_works
+
+    specify "when RR raises an error it raises a failure not an exception" do
+      project = generate_project
+      project.add_test_file do |file|
+        file.add_test_case do |test_case|
+          test_case.add_test <<-EOT
+            object = Object.new
+            mock(object).foo
+          EOT
+        end
+      end
+      result = project.run_tests
+      result.should fail_with_output(/1 failure/)
+    end
+
+    specify "the database is properly rolled back after an RR error" do
+      project = generate_project do |project|
+        project.add_model_and_migration(:person, :people, :name => :string)
+      end
+      project.add_test_file do |file|
+        file.add_test_case do |test_case|
+          test_case.add_test <<-EOT
+            Person.create!(:name => 'Joe Blow')
+            object = Object.new
+            mock(object).foo
+          EOT
+        end
+      end
+      expect {
+        result = project.run_tests
+        result.should be_success
+      }.to leave_database_table_clear(project, :people)
+    end
+
+    specify "throwing an error in teardown doesn't mess things up" do
+      project = generate_project
+      project.add_test_file do |file|
+        file.add_test_case do |test_case|
+          test_case.add_to_body <<-EOT
+            def teardown
+              raise 'hell'
+            end
+          EOT
+          test_case.add_test("")   # doesn't matter
+        end
+      end
+      result = project.run_tests
+      result.should fail_with_output(/1 error/)
+    end
+  end
 end
