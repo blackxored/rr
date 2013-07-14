b1885fab92b5b7446adc77fac3b95114eb6fa123
Elliot Winkler <elliot.winkler@gmail.com>
2013-07-14 03:04:58 -0600

    WIP: Fix other test suites in response to recent changes

--- spec/suites/rspec_2_rails_3/integration/minitest_4_rails_3_spec.rb
+++ spec/suites/rspec_2_rails_3/integration/minitest_4_rails_3_spec.rb
@@ -1,70 +1,64 @@
 require File.expand_path('../../spec_helper', __FILE__)
 require File.expand_path('../../../common/rails_integration_tests', __FILE__)
-require File.expand_path('../../../common/rails_minitest_project', __FILE__)
+require File.expand_path('../../../common/rails_test_unit_like_project', __FILE__)
 require File.expand_path('../../../common/cucumber_project', __FILE__)
 
 describe 'Integration with MiniTest 4 and Rails 3' do
   include RailsIntegrationTests
 
-  def configure_project_creator(creator)
+  def configure_project_generator(project_generator)
     super
-    creator.add RailsMinitestProject
-    creator.configure do |project|
+    project_generator.mixin RailsTestUnitLikeProject
+    project_generator.configure do |project|
       project.rails_version = 3
-      project.minitest_version = '~> 4.0'
+      project.test_dependencies << ['minitest', '~> 4.0']
+      project.test_file_generator.configure do |file|
+        file.requires << 'minitest/autorun'
+      end
     end
   end
 
   def self.including_the_adapter_manually_works(args={})
     specify "including the adapter manually works" do
-      project = create_project
-      file = project.build_test_file <<-EOT
-        class ActiveSupport::TestCase
-          include RR::Adapters::MiniTest
-        end
-
-        class FooTest < ActiveSupport::TestCase
-          def test_the_correct_adapters_are_loaded
-            assert_adapters_loaded #{matching_adapters.inspect}
+      project = generate_project
+      project.add_test_file do |file|
+        file.add_to_body <<-EOT
+          class ActiveSupport::TestCase
+            include RR::Adapters::MiniTest
           end
-
-          def test_foo
-            object = Object.new
-            mock(object).foo
-            object.foo
-          end
+        EOT
+        file.add_working_test_case_with_adapter_tests do |test_case|
+          test_case.add_to_body <<-EOT
+            def test_the_correct_adapters_are_loaded
+              assert_adapters_loaded #{matching_adapters.inspect}
+            end
+          EOT
         end
-      EOT
-      result = project.run_test_file(file)
+      end
+      result = project.run_tests
+      result.should be_success
       result.should have_no_errors_or_failures
     end
   end
 
   def self.rr_hooks_into_the_test_framework_automatically(args={})
     specify "RR hooks into the test framework automatically" do
-      project = create_project
-      file = project.build_test_file <<-EOT
-        class FooTest < ActiveSupport::TestCase
-          include TestUnitLikeAdapterTests
-
-          def test_foo
-            object = Object.new
-            mock(object).foo
-            object.foo
-          end
-        end
-      EOT
-      result = project.run_test_file(file)
+      project = generate_project
+      project.add_test_file do |file|
+        file.add_working_test_case
+      end
+      result = project.run_tests
+      result.should be_success
       result.should have_no_errors_or_failures
     end
   end
 
   def self.using_rr_with_cucumber_works
     specify "using RR with Cucumber works" do
-      project_creator = build_project_creator do |creator|
-        creator.add CucumberProject
+      project_generator = build_project_generator do |project_generator|
+        project_generator.mixin CucumberProject
       end
-      project = project_creator.create
+      project = project_generator.call
       result = project.run_command_within("bundle exec cucumber")
       result.should be_success
     end
@@ -75,9 +69,9 @@ describe 'Integration with MiniTest 4 and Rails 3' do
   # actually load it. So this is not supported.
 
   context 'when RR is being required manually, and RR is required before the test framework' do
-    def configure_project_creator(creator)
+    def configure_project_generator(project_generator)
       super
-      creator.configure do |project|
+      project_generator.configure do |project|
         project.autorequire_gems = false
         project.include_rr_before_test_framework = true
       end
@@ -93,9 +87,9 @@ describe 'Integration with MiniTest 4 and Rails 3' do
   end
 
   context 'when RR is being required manually, and RR is required after the test framework' do
-    def configure_project_creator(creator)
+    def configure_project_generator(project_generator)
       super
-      creator.configure do |project|
+      project_generator.configure do |project|
         project.autorequire_gems = false
         project.include_rr_before_test_framework = false
       end
@@ -111,69 +105,51 @@ describe 'Integration with MiniTest 4 and Rails 3' do
     using_rr_with_cucumber_works
 
     specify "when RR raises an error it raises a failure not an exception" do
-      project = create_project
-      file = project.build_test_file <<-EOT
-        class FooTest < ActiveSupport::TestCase
-          def test_foo
+      project = generate_project
+      project.add_test_file do |file|
+        file.add_test_case do |test_case|
+          test_case.add_test <<-EOT
             object = Object.new
             mock(object).foo
-          end
+          EOT
         end
-      EOT
-      result = project.run_test_file(file)
+      end
+      result = project.run_tests
       result.should fail_with_output(/1 failure/)
     end
 
     specify "the database is properly rolled back after an RR error" do
-      project = create_project do |project|
-        project.add_file 'app/models/person.rb', <<-EOT
-          class Person < ActiveRecord::Base
-            attr_accessible :name
-          end
-        EOT
-        project.add_file "db/migrate/#{Time.now.strftime("%Y%m%d%H%M%S")}_create_people.rb", <<-EOT
-          class CreatePeople < ActiveRecord::Migration
-            def up
-              create_table :people do |t|
-                t.string :name
-              end
-            end
-
-            def down
-              drop_table :people
-            end
-          end
-        EOT
+      project = generate_project do |project|
+        project.add_model_and_migration(:person, :people, :name => :string)
       end
-      file = project.build_test_file <<-EOT
-        class FooTest < ActiveRecord::TestCase
-          def test_one
+      project.add_test_file do |file|
+        file.add_test_case do |test_case|
+          test_case.add_test <<-EOT
             Person.create!(:name => 'Joe Blow')
             object = Object.new
             mock(object).foo
-          end
+          EOT
         end
-      EOT
+      end
       expect {
-        result = project.run_test_file(file)
+        result = project.run_tests
         result.should be_success
       }.to leave_database_table_clear(project, :people)
     end
 
     specify "throwing an error in teardown doesn't mess things up" do
-      project = create_project
-      file = project.build_test_file <<-EOT
-        class FooTest < ActiveRecord::TestCase
-          teardown do
-            raise 'hell'
-          end
-
-          def test_one
-            # whatever
-          end
+      project = generate_project
+      project.add_test_file do |file|
+        file.add_test_case do |test_case|
+          test_case.add_to_body <<-EOT
+            def teardown
+              raise 'hell'
+            end
+          EOT
+          test_case.add_test("")   # doesn't matter
         end
-      EOT
-      result = project.run_test_file(file)
+      end
+      result = project.run_tests
       result.should fail_with_output(/1 error/)
     end
   end
